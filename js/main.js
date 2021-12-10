8a0c3e3afc88d03249cd39a064296217535c556b
Bonnie DiPasquale <bonnie.the.builder@icloud.com>
2021-12-10 17:12:59 -0500

    show app functionality in pseudocode

--- js/main.js
+++ js/main.js
@@ -14,3 +14,36 @@ const Quiz = function app() {
 
 const newTest = Quiz();
 newTest.getQuestions();
+
+/**
+ * Pseudocode
+ * 
+ * Start App initializes the first screen where user presses 'start quiz'
+ * 
+ * attach listener to 'start quiz' -- run startQuiz function
+ * 
+ * startQuiz()
+ * - initializes countdown timer
+ * - shows progress bar
+ * - initializes _score
+ * - initializes question counter to store which question user is on
+ * - loads first question & answers
+ * - starts countdown timer
+ * 
+ * renderQuestion()
+ * - display question, answers and answer button
+ * 
+ * answerQuestion()
+ * - runs on answer click
+ * - updates score
+ * - updates timer if the answer is wrong
+ * - displays message to the user if the answer is correct or wrong
+ * - advances to the next question
+ * 
+ * finishQuiz()
+ * - show final score
+ * - show input for initials
+ * - show submit button
+ * - validate input text
+ * 
+ */
